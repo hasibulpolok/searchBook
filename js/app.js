8cf1fbe2786a77bb95d2b785606881e946345534
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-09-02 03:48:50 +0600

    all functionality done

--- js/app.js
+++ js/app.js
@@ -1,21 +1,48 @@
- const searchFun = () => {
-     const searchField = document.getElementById('searchfield');
-     const searchText = searchField.value; 
-     searchField.value = "";
-     const url=(`https://openlibrary.org/search.json?q=${searchText}`);
-     console.log(url);
-     fetch(url)
-     .then(res => res.json())
-     .then(data => displaySearchBooks(data.docs));
- }
+// search button function 
 
- const displaySearchBooks = books => {
-     const showResults = document.getElementById('results');
-     showResults.textContent = '';
-     books.forEach(book =>{
-         const div = document.createElement('div');
-         div.classList.add('col');
-         div.innerHTML =`
+const searchFun = () => {
+    const searchField = document.getElementById('searchfield');
+    const searchText = searchField.value;
+    searchField.value = "";
+
+    // displayspinner 
+    toggleSpinner('block');
+
+    const url = (`https://openlibrary.org/search.json?q=${searchText}`);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchBooks(data.docs));
+
+}
+
+
+// spinner function
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
+// display search result 
+
+const displaySearchBooks = books => {
+    const showResults = document.getElementById('results');
+    showResults.textContent = '';
+
+    //  result not found 
+    if (books.length == 0) {
+        document.getElementById('nomatch').style.display = 'block'
+    }
+
+    // number of result found 
+
+    if (books.length > 0) {
+        document.getElementById('totalresult').innerText = `Total ${books.length} match found`
+        books.length
+    }
+
+    books.forEach(book => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
          <div class="bookdesk">
                 <div class="card-group">
                     <div class="card mx-2">
@@ -26,15 +53,23 @@
                             <h6 class="card-title text-center">Author: ${book.author_name}</h6>
                             <h6 class="card-title text-center">Published on: ${book.publish_date}
                             </h6>
+                            <h6 class="card-title text-center">First Publish: ${book.first_publish_year}
+                            </h6>
                             <h6 class="card-title text-center">Language: ${book.language}
                             </h6>
                             <h6 class="card-title text-center">Publisher: ${book.publisher}
                             </h6>
+                            
                         </div>
                     </div>
                 </div>
             </div>
          `;
-         showResults.appendChild(div);
-     })
- }
+
+        showResults.appendChild(div);
+    });
+    //  hide spinner 
+    toggleSpinner('none')
+
+}
+
